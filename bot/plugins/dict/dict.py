1547577c94ecb34248468a7d696dfafb31930c60
AbhijithNT <abhijithnt64@gmail.com>
2021-09-19 15:27:54 +0530

    text space removed

--- bot/plugins/dict/dict.py
+++ bot/plugins/dict/dict.py
@@ -1,13 +1,13 @@
 from bot.olambot import OlamBot, filters
 from bot.helpers.malayalam_dict import malayalamDictBot
 
+
 @OlamBot.on_message()
 async def malayalamDict(client, msg):
     word = malayalamDictBot(msg.text)
     w = f"\n".join(word)
     await client.send_message(
         chat_id=msg.chat.id,
-        text=f" {msg.text.capitalize()}\n {w}",
+        text=f"{msg.text.capitalize()}\n{w}",
         parse_mode="html"
     )
-
