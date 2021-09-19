2d521086fe5c838c20f6889d2fa21663324173a0
AbhijithNT <abhijithnt64@gmail.com>
2021-09-19 15:27:12 +0530

    message added

--- bot/plugins/cmd/start.py
+++ bot/plugins/cmd/start.py
@@ -1,9 +1,10 @@
 from pyrogram.filters import command
 from ...olambot import OlamBot, filters
+from .message import DEV, START, SOURCE
 
 
 @OlamBot.on_message(filters.command("start"))
 async def onStart(_, msg):
     await msg.reply(
-        "hi"
+        f"Hy <b>{msg.from_user.first_name},{START}{DEV}{SOURCE}</b>"
     )
