b9020d0895705a54fd403a33f07c3cd80a116748
abhint <notifyabhijith@gmail.com>
2022-11-27 10:46:28 +0530

    reformat

--- bot/plugins/cmd/message.py
+++ bot/plugins/cmd/message.py
@@ -1,4 +1,5 @@
-START = "\nThis bot is an English-Malayalam dictionary. This bot is built from Olam open-source data. Olam is an online website for English-Malayalam dictionaries.\n"
+START = "\nThis bot is an English-Malayalam dictionary. This bot is built from Olam open-source data. Olam is an " \
+        "online website for English-Malayalam dictionaries.\n"
 DEV = "\nDeveloper: @abhint"
 SOURCE = "\nSource: https://github.com/abhint/OlamTelegramBot"
 
