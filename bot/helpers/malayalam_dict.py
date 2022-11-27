b5b8336c0f8ec25116224bfa95195c90270eb6ce
abhint <notifyabhijith@gmail.com>
2022-11-27 10:35:35 +0530

    Function to search words from the database.

--- bot/helpers/malayalam_dict.py
+++ bot/helpers/malayalam_dict.py
@@ -7,23 +7,14 @@ parts_speech = open('data/parts-of-speech.json', 'r')
 load_ps = json.loads(parts_speech.read())
 
 
-def do_search(user_text):
-    word: str
-    word_id: str
-    result = []
-    ps_result = []
-    sql_serch_id = cur.execute(
-        f"SELECT word, _id FROM words_en WHERE stems LIKE '%{user_text}%' OR stems LIKE '{user_text}' ORDER BY "
-        f"LENGTH(word) LIMIT 1")
-    for row in sql_serch_id:
-        word, word_id = row
-    sql_search_dict = cur.execute(
-        f'SELECT word, id_en,type FROM relations_en_ml LEFT JOIN words_ml ON (words_ml._id = relations_en_ml.id_ml) '
-        f'WHERE id_en IN ({word_id}) ORDER BY LENGTH(word)')
-    for row in sql_search_dict:
-        word_dict, _, p_speech = row
-        if p_speech in load_ps:
-            ps_result.append(f"◦ {word_dict} - {load_ps[p_speech]['en']}")
-        else:
-            result.append(f"-{word_dict}")
-    return word, ps_result, result
+def searchWord(word: str):
+    search_query = cur.execute(f"SELECT english_word, part_of_speech, malayalam_definition FROM enml WHERE "
+                               f"english_word LIKE '{word}'")
+    search_result = search_query.fetchall()
+    if not search_result:
+        raise Exception("Word not fund")
+        return
+    search_word = search_result[0][0]
+    search_definition = [f'{words[2]} - {load_ps[words[1]]["en"]}' for words in search_result if words[1] in load_ps]
+    search_entries = [f'{word[2]}' for word in search_result if word[1] not in load_ps]
+    return search_word, search_entries, search_definition
